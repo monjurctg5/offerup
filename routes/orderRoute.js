66fd81aeb43bcf65f5edd22e8a33b8ab987359a7
MD Monjurul Alam <62030644+monjurctg5@users.noreply.github.com>
2022-08-28 16:24:38 +0600

    Update orderRoute.js

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -18,12 +18,16 @@ router.get("/all-order", allOrder);
 router.delete("/delete-order/:id", deleteOrder);
 
 router.get("/", (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
-  res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader.Accept('application/json');
+
+  res.setHeader.post['Content-Type'] ='application/json;charset=utf-8'; // If needed
+  res.setHeader('Access-Control-Allow-Credentials', true); 
 
   res.send('cors problem fixed:)');
+
 });
 
 module.exports = router;
