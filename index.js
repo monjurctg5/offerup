6da943a30650c791d1710ee2e31a52ec77a9c904
MD Monjurul Alam <62030644+monjurctg5@users.noreply.github.com>
2022-08-28 17:49:52 +0600

    Update index.js

--- index.js
+++ index.js
@@ -18,21 +18,20 @@ const PORT = process.env.PORT || 5000;
 //    origin:"http://localhost:5000/api/"
 // }))
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }))
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(express.json())
 
 
-app.use(cors())
+app.use(cors());
+// app.use(cors({
 
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
+// }))
+// app.use(cors({
+
+//    origin:"*"
+// }));
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
