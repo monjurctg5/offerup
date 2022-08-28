0e2db082948675b1d2282aa5b2d429130da66cce
MD Monjurul Alam <62030644+monjurctg5@users.noreply.github.com>
2022-08-28 17:41:34 +0600

    Update ProductModels.js

--- models/ProductModels.js
+++ models/ProductModels.js
@@ -1,31 +1,33 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const ProductSchema = new Schema({
-    productName:String,
-    title:String,
-    sortTitle:String,
-    price:Number,
-    description:String,
-    image1:String,
-    image2:String,
-    image3:String,
-    image4:String,
-    condition:String,
-    category:String,
-    shippingCost:String,
-    place:String,
-    active:{
-        type:Boolean,default:false
-    }
- 
-   
-},{
+const ProductSchema = new Schema(
+  {
+    productName: { type: String, require: true, default: "" },
+    title: { type: String, require: true, default: "" },
+    sortTitle: { type: String, require: true, default: "" },
+    price: { type: Number, require: true, default: 0 },
+    description: { type: String, require: true, default: "" },
+    image1: { type: String, require: true, default: "" },
+    image2: { type: String, default: "" },
+    image3: { type: String, default: "" },
+    image4: { type: String, default: "" },
+    condition: { type: String, default: "new" },
+    category: { type: String, require: true, default: "" },
+    shippingCost: { type: Number, default: 0 },
+    place: { type: String, default: "" },
+    active: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
     timestamps: {
-        createdAt: 'created_at', // Use `created_at` to store the created date
-        updatedAt: 'updated_at' // and `updated_at` to store the last updated date
-      }
-})
-const Product = mongoose.model('Products',ProductSchema);
+      createdAt: "created_at", // Use `created_at` to store the created date
+      updatedAt: "updated_at", // and `updated_at` to store the last updated date
+    },
+  }
+);
+const Product = mongoose.model("Products", ProductSchema);
 
-module.exports = Product
+module.exports = Product;
