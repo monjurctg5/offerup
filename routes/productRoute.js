4b42c4b9b9c49cac97916cbc41d86c8ea500cfa7
MD Monjurul Alam <62030644+monjurctg5@users.noreply.github.com>
2022-08-28 16:40:00 +0600

    Update productRoute.js

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -26,10 +26,13 @@ router.get("/active-product", activeProduct);
 router.delete("/delete-product/:id", deleteProduct);
 
 router.get("/", (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
-  res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true); 
+  res.setHeader('Accept','application/json');
+
+  res.setHeader('Content-Type','application/json;charset=utf-8') // If needed
 
   res.send('cors problem :)');
 });
